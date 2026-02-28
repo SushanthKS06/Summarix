5ce27da5c689e352ad7babc706709898dfc24a2c
Momentum-666 <k.s.sushanth06@gmail.com>
2026-02-28 09:44:52 +0530

    fix: limit celery worker concurrency to 1

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start Celery worker in the background
-celery -A app.core.celery_app.celery_app worker --loglevel=info &
+celery -A app.core.celery_app.celery_app worker --loglevel=info --concurrency=1 &
 
 # Start Uvicorn in the foreground
 exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
